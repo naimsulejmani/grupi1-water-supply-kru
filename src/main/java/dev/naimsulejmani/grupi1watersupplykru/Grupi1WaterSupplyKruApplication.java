42b22ffe5f3abc56d2c2d0ec32351bd950ccdc63
Naim Sulejmani <naim.sulejmani@gmail.com>
2024-11-21 14:05:17 +0100

    layout basic and dependencies

--- src/main/java/dev/naimsulejmani/grupi1watersupplykru/Grupi1WaterSupplyKruApplication.java
+++ src/main/java/dev/naimsulejmani/grupi1watersupplykru/Grupi1WaterSupplyKruApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Grupi1WaterSupplyKruApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Grupi1WaterSupplyKruApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Grupi1WaterSupplyKruApplication.class, args);
+    }
 
 }
