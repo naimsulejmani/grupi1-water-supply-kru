bfdb537deee0b4b68bfadc2be6b7f4ea0f9fbfe9
Naim Sulejmani <naim.sulejmani@gmail.com>
2024-11-27 14:05:46 +0100

    customer slist/add edhe edit (editi jo bash mire)

--- src/main/java/dev/naimsulejmani/grupi1watersupplykru/controllers/CustomerController.java
+++ src/main/java/dev/naimsulejmani/grupi1watersupplykru/controllers/CustomerController.java
@@ -1,10 +1,10 @@
 package dev.naimsulejmani.grupi1watersupplykru.controllers;
 
+import dev.naimsulejmani.grupi1watersupplykru.models.Customer;
 import dev.naimsulejmani.grupi1watersupplykru.services.CustomerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/customers")
@@ -22,6 +22,30 @@ public class CustomerController {
         return "customers/index";
     }
 
+    @GetMapping("/new")
+    public String newCustomer(Model model) {
+        model.addAttribute("customer", new Customer());
+        return "customers/new";
+    }
+
+    @PostMapping("/new")
+    public String newCustomer(@ModelAttribute Customer customer) {
+        customerService.add(customer);
+        return "redirect:/customers";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String editCustomer(Model model, @PathVariable Long id) {
+        var customer = customerService.findById(id);
+        model.addAttribute("customer", customer);
+        return "customers/edit";
+    }
+
+    @PostMapping("/{id}/edit")
+    public String editCustomer(@ModelAttribute Customer customer) {
+        customerService.modify(customer);
+        return "redirect:/customers";
+    }
 
 }
 
