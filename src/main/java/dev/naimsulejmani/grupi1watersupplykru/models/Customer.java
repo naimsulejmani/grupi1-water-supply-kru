382492ec17f3707f50ab2eac7923efb7a99dc7a3
Naim Sulejmani <naim.sulejmani@gmail.com>
2024-12-03 12:59:17 +0100

    FYI

--- src/main/java/dev/naimsulejmani/grupi1watersupplykru/models/Customer.java
+++ src/main/java/dev/naimsulejmani/grupi1watersupplykru/models/Customer.java
@@ -21,40 +21,59 @@ public class Customer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Min(value = 0, message = "ID duhet te jete numer pozitiv ose zero!")
+//    @Min(value = 0, message = "ID duhet te jete numer pozitiv ose zero!")
+    @PositiveOrZero(message = "ID duhet te jete numer pozitiv ose zero!")
     private long id;
 
     @Column(nullable = false, length = 50)
     @Size(min = 3, max = 50, message = "Emri duhet te jete me shkruajt 3 deri ne 50 karaktere!")
+    @NotBlank(message = "Emri nuk mund te jete i zbrazet!")
+    @NotNull(message = "Emri nuk mund te jete null!")
 //    @NotBlank
 //    @NotEmpty
 //    @NotNull
     private String firstName;
     @Column(nullable = false, length = 50)
     @Size(max = 50)
+    @NotBlank
     private String lastName;
 
     @Column(length = 500)
     @Size(max = 500)
     private String companyName;
-    @Column(nullable = false, length = 500)
-    private String address;
-    @Column(nullable = false, length = 100)
-    private String city;
-    @Column(nullable = false, length = 100)
-    private String state;
-    @Column(nullable = false, length = 10)
 
+    @Column(nullable = false, length = 500)
+    @Size(max = 500)
+    private String address;
+
+    @Column(nullable = false, length = 100)
+    @Size(max = 100, message = "Qyteti duhet te jete me shkruajt deri ne 100 karaktere!")
+    private String city;
+
+    @Column(nullable = false, length = 100)
+    @Size(max = 100, message = "Shteti duhet te jete me shkruajt deri ne 100 karaktere!")
+    private String state;
+
+    @Column(nullable = false, length = 10)
     @Size(min = 5, max = 10, message = "Zipi duhet te jete 5 deri ne 10 karaktere")
     private String zip;
+
     @Column(length = 50)
     @Email(message = "Emaila duhet me qene valide")
+    @Pattern(regexp = "^(.+)@(.+)$", message = "Emaila duhet te permbaje @")
+    @Size(max = 50)
     private String email;
+
     @Column(length = 25)
+    @Size(max=25)
     private String phone;
+
     @Column(nullable = false)
     @PastOrPresent(message = "Data e regjistrimit duhet te jete ne te kaluaren ose ne diten e sotme")
+    @NotNull
     private LocalDate registeredDate;
+
+
     @Column(nullable = false)
     private boolean active = true;
 
@@ -62,8 +81,11 @@ public class Customer {
     private LocalDateTime createdAt = LocalDateTime.now();
     @Column(nullable = false, length = 50, updatable = false)
     private String createdBy = "admin";
+
     private LocalDateTime modifiedAt;
+
     @Column(length = 50)
+    @Size(max = 50)
     private String modifiedBy;
 
 
