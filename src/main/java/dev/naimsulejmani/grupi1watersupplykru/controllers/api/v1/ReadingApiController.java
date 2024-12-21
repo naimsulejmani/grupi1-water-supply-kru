1bbc330e6d76f0f747f06d9fae90e094279f8b29
Naim Sulejmani <naim.sulejmani@gmail.com>
2024-12-21 11:49:25 +0100

    Started Rest

---
+++ src/main/java/dev/naimsulejmani/grupi1watersupplykru/controllers/api/v1/ReadingApiController.java
@@ -0,0 +1,10 @@
+package dev.naimsulejmani.grupi1watersupplykru.controllers.api.v1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/readings")
+public class ReadingApiController {
+
+}
